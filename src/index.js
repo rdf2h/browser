5f6e299d2c79cd9edd630c5ba6fd2158b7d99485
Reto Gmür <reto@factsmission.com>
2018-02-14 22:23:01 +0100

    Getting data from defined sparql endpoints

--- src/index.js
+++ src/index.js
@@ -6,15 +6,22 @@ import $rdf from "rdflib";
 //returns a promise for the graph
 function loadData(uri) {
     function routes() {
-        return Array.from(document.getElementsByClassName("route")).map((div) => {
-            let route = {};
-            route.endPoint = div.getElementsByClassName('sparql-endpoint')[0].value;
-            route.pattern = new RegExp(div.getElementsByClassName('uri-pattern')[0].value,"yi");
-            route.loadData = function(uri) {
-                alert("cant load "+uri+" with "+route.endPoint);
-            };
-            return route;
-        });
+        return Array.from(document.getElementsByClassName("route")).map((div) => ({
+            'endPoint' : div.getElementsByClassName('sparql-endpoint')[0].value,
+            'pattern' : new RegExp(div.getElementsByClassName('uri-pattern')[0].value,"yi"),
+            'loadData' : function(uri) {
+                console.log("loading "+uri+" with "+this.endPoint);
+                let query = `DESCRIBE <${uri}>`;
+                return GraphNode.rdfFetch(this.endPoint + "?query=" + encodeURIComponent(query)).then(response =>
+                {
+                    if (!response.ok) {
+                        throw response.status;
+                    } else {
+                        return response.graph;
+                    }
+                });
+            }
+        }));
     }
     let route = routes().find((route) => route.pattern.test(uri));
     if (route) {
@@ -24,15 +31,21 @@ function loadData(uri) {
 }
 
 document.getElementById("lookup").onsubmit = () => {
-    let rus = document.getElementsByClassName("renderer-uri");
-    let loadPromises = [];
-    let renderers = $rdf.graph();
-    let dataGraph;
-    for (let i = 0; i < rus.length; i++) {
-        loadPromises.push(GraphNode.rdfFetch(rus[i].value).then((res) => renderers.addAll(res.graph.statements)));
-    };
-    let uri = document.getElementById("uri").value;
-    loadPromises.push(loadData(uri).then((g) => dataGraph = g));
-    Promise.all(loadPromises).then(() => document.getElementById("results").innerHTML = new RDF2h(renderers).render(dataGraph, uri));
+    try {
+        let rus = document.getElementsByClassName("renderer-uri");
+        let loadPromises = [];
+        let renderers = $rdf.graph();
+        let dataGraph;
+        for (let i = 0; i < rus.length; i++) {
+            loadPromises.push(GraphNode.rdfFetch(rus[i].value).then((res) => renderers.addAll(res.graph.statements)));
+        };
+        let uri = document.getElementById("uri").value;
+        loadPromises.push(loadData(uri).then((g) => dataGraph = g));
+        Promise.all(loadPromises).then(() => {
+            document.getElementById("results").innerHTML = new RDF2h(renderers).render(dataGraph, uri)
+        });
+    } catch(e) {
+        console.error("Exception processing settings",e);
+    }
     return false;
 };
