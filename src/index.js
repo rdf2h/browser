abd0cc1debdb75f25d029a5e5e9109d41331d450
Reto Gmür <reto@factsmission.com>
2018-02-15 11:22:29 +0100

    added waitng wheel

--- src/index.js
+++ src/index.js
@@ -30,31 +30,34 @@ function loadData(uri) {
 }
 
 function showResource() {
-    try {
-        let rus = document.getElementsByClassName("renderer-uri");
-        let loadPromises = [];
-        let renderers = $rdf.graph();
-        let dataGraph;
-        for (let i = 0; i < rus.length; i++) {
-            loadPromises.push(GraphNode.rdfFetch(rus[i].value).then((res) => renderers.addAll(res.graph.statements)));
-        };
-        let uri = document.getElementById("uri").value;
-        loadPromises.push(loadData(uri).then((g) => dataGraph = g));
-        Promise.all(loadPromises).catch((e) => alert(e)).then(() => {
-            let resultsElement = document.getElementById("results");
-            resultsElement.innerHTML = new RDF2h(renderers).render(dataGraph, uri);
-            let links = Array.from(resultsElement.getElementsByTagName('a'));
-            links.forEach((link) => {
-                link.onclick = function () {
-                    document.getElementById("uri").value = link.href;
-                    showResource();
-                    return false;
-                }
+    let resultsElement = document.getElementById("results");
+    setTimeout(() => {
+        try {
+            resultsElement.innerHTML  = "<img src='waiting-wheel-300x300.gif' alt='please wait' />";
+            let rus = document.getElementsByClassName("renderer-uri");
+            let loadPromises = [];
+            let renderers = $rdf.graph();
+            let dataGraph;
+            for (let i = 0; i < rus.length; i++) {
+                loadPromises.push(GraphNode.rdfFetch(rus[i].value).then((res) => renderers.addAll(res.graph.statements)));
+            };
+            let uri = document.getElementById("uri").value;
+            loadPromises.push(loadData(uri).then((g) => dataGraph = g));
+            Promise.all(loadPromises).catch((e) => alert(e)).then(() => {
+                resultsElement.innerHTML = new RDF2h(renderers).render(dataGraph, uri);
+                let links = Array.from(resultsElement.getElementsByTagName('a'));
+                links.forEach((link) => {
+                    link.onclick = function () {
+                        document.getElementById("uri").value = link.href;
+                        showResource();
+                        return false;
+                    }
+                });
             });
-        });
-    } catch(e) {
-        console.error("Exception processing settings",e);
-    }
+        } catch(e) {
+            console.error("Exception processing settings",e);
+        }
+    }, 1);
 }
 
 document.getElementById("lookup").onsubmit = () => {
